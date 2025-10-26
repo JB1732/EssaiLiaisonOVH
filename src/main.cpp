7d384c4d196c4ce84ed096681a4ed0e1f1e323e0
JB1732 <jb@cohala.fr>
2025-10-26 11:53:14 +0100

    Commit2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,73 @@
-#include <Arduino.h>
+// Import required libraries
+#include "WiFi.h"
+#include <AsyncTCP.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "Freeboxjb";
+const char *password = "coucou123";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+
+void setup()
+{
+  //.....................Liaison série..............
+  Serial.begin(115200);
+
+   //--------------------------SPIFFS................................
+  Serial.println("Debut SPIFFS");
+  delay(500);
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+  else
+    Serial.println("SPIFFS OK");
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  Serial.println("Parcours du SPIFFS");
+  delay(500);
+
+  while (file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+  Serial.println("Fin SPIFFS");
+  delay(500);
+
+  
+  // Connect to Wi-Fi
+      WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Adresse IP de l'ESP32 : " + WiFi.localIP().toString());
+  Serial.println("\n");
+  Serial.println(".....................ESP32-C6.......................");
+  Serial.println(".............. .............");
+  Serial.println("\n");
+
+   //--------------------------SERVER-----------------------------------
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", "text/html"); });
+
+  // server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
+  //           { request->send(200, "text/plain", readDHTTemperature().c_str()); });
+   server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y + 1 ;
+void loop()
+{
 }
